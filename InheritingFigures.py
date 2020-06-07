b2b81b101478f782292683626e16e28d972c25db
Artur Leszczynski <artles00@wp.pl>
2020-06-07 15:18:29 +0200

    First Project

---
+++ InheritingFigures.py
@@ -0,0 +1,35 @@
+class Rectangle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def count_area(self):
+        return self.x * self.y
+
+
+class Square(Rectangle):
+    def __init__(self, z):
+        self.z = z
+        super().__init__(z, z)
+
+
+class Cube():
+    def __init__(self, square: Square):
+        self.square = square
+        self.x = square.x
+
+    def count_area(self):
+        return self.square.count_area() * 6
+
+    def count_volume(self):
+        return self.square.count_area() * self.x
+
+
+class Cuboid():
+    def __init__(self, figure, height):
+        self.base = figure
+        self.height = height
+
+    def count_volume(self):
+        return self.base.count_area() * self.height
+
